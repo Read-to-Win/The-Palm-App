5fbd7812e0ae797ef0bdbe5f1fe13f5d73061db1
Patience Grace Affel <gpatience14@gmail.com>
2025-06-19 11:28:04 +0000

    first push

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Hero from "../components/Hero";
 
 const Landing = () => {
   return (
